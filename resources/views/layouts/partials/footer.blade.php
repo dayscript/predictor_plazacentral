c680771e08677dcb0c1c33f8552a7e50d577db5a
jcardenas2015 <jcardenas@dayscript.com>
2018-06-13 11:36:46 -0500

    cambio estilos

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,16 +1,16 @@
 <div class="footer">
     <div class="row">
-        <div class="medium-2 columns">
+        <div class="medium-3 columns">
             <div class="img-logo">
                 <img class="logo" src="/img/logo-predictor.png" alt="Predictor">
             </div>
         </div>
-        <div class="medium-2 columns">
+        <div class="medium-3 columns">
             <div class="img-logo">
                 <img class="logo" src="/img/univision.png" alt="Univision">
             </div>
         </div>
-        <div class="medium-8 columns">
+        <div class="medium-6 columns">
             <ul>
                 <li>@{{ $store.getters.trans('footer.copyright') }} &copy; {{ date('Y') }} {{ config('app.name', 'Predictor') }} </li>
                 <li><a href="terms-and-conditions">@{{ $store.getters.trans('footer.terms_and_conditions') }}</a></li>
