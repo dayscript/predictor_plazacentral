68fdb4ef08e6c56a28a82b1c63a03e040b06e6a4
jcardenas2015 <jcardenas@dayscript.com>
2018-07-09 17:54:45 -0500

    cuartos

--- resources/assets/js/components/predictions/PredictionsIndex.vue
+++ resources/assets/js/components/predictions/PredictionsIndex.vue
@@ -39,11 +39,11 @@
                             <a href="#panel1c">{{ $store.getters.trans('predictions.group_phase') }}</a>
                         </li>
                         <li class="tabs-title">
-                            <a href="#panel2c">{{ $store.getters.trans('game.round_round_of_16') }}</a></li>
+                            <a href="#panel2c" aria-selected="true">{{ $store.getters.trans('game.round_round_of_16') }}</a></li>
                         <li class="tabs-title">
-                            <a href="#panel3c" aria-selected="true">{{ $store.getters.trans('game.round_quarter_finals') }}</a></li>
-                        <li class="tabs-title">
-                            <a href="#panel4c is-active">{{ $store.getters.trans('game.round_semi_finals') }}</a></li>
+                            <a href="#panel3c">{{ $store.getters.trans('game.round_quarter_finals') }}</a></li>
+                        <li class="tabs-title is-active">
+                            <a href="#panel4c">{{ $store.getters.trans('game.round_semi_finals') }}</a></li>
                         <li class="tabs-title"><a href="#panel5c">{{ $store.getters.trans('game.round_finals') }}</a>
                         </li>
                     </ul>
